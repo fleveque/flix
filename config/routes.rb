71599479979d413572f76bee4bbf517aec7938b9
Francesc Leveque <francesc@leveque.es>
2025-09-02 15:21:47 +0000

    Chapter 11: Linking pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "movies#index"
   get "movies" => "movies#index"
-  get "movies/:id" => "movies#show"
+  get "movies/:id" => "movies#show", as: "movie"
 end
