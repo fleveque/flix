5846f1b40ba038f6c1d939d1b329bc1e2551a789
Francesc Leveque <francesc@leveque.es>
2025-09-03 09:15:07 +0000

    Chapter 12: Forms: Editing records. Edit form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,19 @@
 class MoviesController < ApplicationController
+  before_action :find_movie, only: [ :show, :edit ]
+
   def index
     @movies = Movie.all
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def find_movie
     @movie = Movie.find(params[:id])
   end
 end
