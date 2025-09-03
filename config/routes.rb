ec16478032eb058a2a0abf0810a977c5d5b41eb0
Francesc Leveque <francesc@leveque.es>
2025-09-03 10:39:09 +0000

    Chapter 13: Creating records

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "movies#index"
-  get "movies" => "movies#index"
-  get "movies/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-  patch "movies/:id" => "movies#update"
+
+  resources :movies
+  # get "movies" => "movies#index"
+  # get "movies/new" => "movies#new", as: "new_movie"
+  # get "movies/:id" => "movies#show", as: "movie"
+  # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  # patch "movies/:id" => "movies#update"
 end
