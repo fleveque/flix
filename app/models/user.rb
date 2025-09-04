ffecd718afe7b378dff3736d183ba284b3a8f877
Francesc Leveque <francesc@leveque.es>
2025-09-04 19:19:10 +0000

    Chapter 26: User account model. Fix validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :email, format: { with: /\S+@\S+/ },
+  validates :email, format: { with: /\A\S+@\S+\z/ },
             uniqueness: { case_sensitive: false }
   # validates :password, length: { minimum: 10, allow_blank: true }
 end
