03366d26caf3124861de9f529fbed8ced9fa8ba1
Francesc Leveque <francesc@leveque.es>
2025-09-04 08:11:00 +0000

    Chapter 22: One-to-Many: has_many

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :movie
+
+  STARS = (1..5).to_a
+
+  validates :name, presence: true
+  validates :stars, inclusion: {
+    in: STARS,
+    message: "must be between 1 and 5"
+  }
+  validates :comment, length: { minimum: 4 }
 end
